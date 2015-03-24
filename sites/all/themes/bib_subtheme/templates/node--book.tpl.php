bb3ff7b13ca72c5d16e827fb8f91c8f3421f4205
unknown <jeroeno@HI37511.cegekanv.corp.local>
2015-03-24 10:48:35 +0100

    Story 3 commit

--- sites/all/themes/bib_subtheme/templates/node--book.tpl.php
+++ sites/all/themes/bib_subtheme/templates/node--book.tpl.php
@@ -80,35 +80,9 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-    <?php print(render($content['field_isbn'])) ?>
-    <?php print_r(array_keys($content)); ?>
-
-    <?php print render($title_prefix); ?>
-    <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>>
-            <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-        </h2>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-
-    <?php if ($display_submitted): ?>
-        <div class="meta submitted">
-            <?php print $user_picture; ?>
-            <?php print $submitted; ?>
-        </div>
-    <?php endif; ?>
-
-    <div class="content clearfix"<?php print $content_attributes; ?>>
-        <?php
-
-        // We hide the comments and links now so that we can render them later.
-        hide($content['comments']);
-        hide($content['links']);
-        show($content['field_isbn']);
-        print render($content);
-        ?>
-    </div>
-
+    <div class ='bookcover'><?php print(render($content['field_book_cover'])); ?></div>
+    <div class ='author'> <?php print(t('Author'));?>: <?php print(render($content['field_first_name'])); ?><?php print(render($content['field_last_name'])); ?> </div>
+    <div class ='isbn'><?php print(render($content['field_isbn'])); ?></div>
 
 
 </div>
